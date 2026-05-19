8114f19d4672082d3f64bbedd8adc0b99f49ce34
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-05-19 10:24:08 -0400

    replaced an element

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = ()
+    new_list = []
     for item in my_list:
         if item == replace:
             new_list.append(search)
