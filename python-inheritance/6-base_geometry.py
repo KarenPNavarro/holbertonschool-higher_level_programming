30f4a38defea6ec246b9b15f40b453f4f742fd55
Karen Navarro <karennavarrofigueroa@protonmail.com>
2026-05-31 12:51:11 -0400

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry with an area method."""
+
+
+class BaseGeometry:
+    """Represents base geometry."""
+
+    def area(self):
+        """Raises an Exception: area is not implemented."""
+        raise Exception("area() is not implemented")
