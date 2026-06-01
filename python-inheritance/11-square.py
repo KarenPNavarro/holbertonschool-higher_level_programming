17db18df78ce50b92312137f120833d0a7d0e56b
Karen Navarro <karennavarrofigueroa@protonmail.com>
2026-05-31 21:43:26 -0400

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -11,3 +11,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
+    def __str__(self):
+        """Returns the [Square] <width>/<height> description."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
