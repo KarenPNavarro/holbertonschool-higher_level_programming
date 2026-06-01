b6cdb322cb21ee34114e0b5f952c62f29a2ffd36
Karen Navarro <karennavarrofigueroa@protonmail.com>
2026-05-31 22:03:40 -0400

    task 4

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Demonstrates multiple inheritance and MRO with a FlyingFish."""
+
+
+class Fish:
+    """Represents a fish."""
+
+    def swim(self):
+        """Prints that the fish is swimming."""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Prints the fish's habitat."""
+        print("The fish lives in water")
+
+
+class Bird:
+    """Represents a bird."""
+
+    def fly(self):
+        """Prints that the bird is flying."""
+        print("The bird is flying")
+
+    def habitat(self):
+        """Prints the bird's habitat."""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Inherits from both Fish and Bird."""
+
+    def fly(self):
+        """Prints that the flying fish is soaring."""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """Prints that the flying fish is swimming."""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """Prints the flying fish's habitat."""
+        print("The flying fish lives both in water and the sky!")
