ce1b97a8af8ce4de5a358e3e02a73c31ab7bc583
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-06-07 19:59:23 -0400

    Write a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """Module that defines write_file function."""
 
 
-def write_file(filename=""):
+def write_file(filename="", text=""):
     """Write a text file and print it."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.write(), end="")
+    with open(filename, "w" encoding="utf-8") as f:
+        return f.write(text)
