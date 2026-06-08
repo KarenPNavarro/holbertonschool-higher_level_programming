83b2c105e50c54a781eb11eafeb8fb44131f9591
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-06-07 20:22:03 -0400

    to JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that defines write_file function."""
+
+
+import json
+json.dumps(my_obj)
+
+
+def to_json_string(my_obj):
+    """"Return JSON representation of an object."""
+    return f.write(my_obj)
