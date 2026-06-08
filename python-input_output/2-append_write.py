4296f5c210d9e2fe2c9ab251031f5ffcd4e987ac
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-06-07 20:13:31 -0400

    Append text

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module that defines write_file function."""
 
+
 def append_write(filename="", text=""):
     """Write a function that appends a string at the end of a text file"""
-    with open(filename="", text="")as f:
-        return.append(text)
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
