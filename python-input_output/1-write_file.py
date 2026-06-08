3818ead9881a8d360ca6bd25d1829676547051c6
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-06-07 20:00:38 -0400

    Write a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Write a text file and print it."""
-    with open(filename, "w" encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
