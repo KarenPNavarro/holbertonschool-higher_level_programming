8df28691aa5846f8d4989c410e72232f89f5fdac
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-07-05 22:46:52 -0400

    fisrt state model

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines the State class and Base using SQLAlchemy ORM."""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class mapped to MySQL table states."""
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
